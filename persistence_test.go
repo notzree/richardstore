4d1b5aef61597cfa74db1685ed0a589963a7e3a3
notzree <notzree@gmail.com>
2024-12-20 15:41:54 -0500

    Building out persistence apis

---
+++ persistence_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLocalMetadataPersistor(t *testing.T) {
+	original_data := &PersistedRaftFields{
+		CurrentTerm: 4,
+		VotedFor:    2,
+	}
+	p := NewFsSnapShotStore[PersistedRaftFields]("test/data.gob")
+	if err := p.Persist(*original_data); err != nil {
+		log.Printf("error persisting data: %s", err)
+	}
+
+	retrieved_data, err := p.GetMostRecent()
+	if err != nil {
+		log.Printf("error retrieving data %s", err)
+	}
+	assert.Equal(t, original_data, retrieved_data)
+}
