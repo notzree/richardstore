2ac67b6f4d8a07cc8ec90b43c68b6f2f622e56f1
notzree <notzree@gmail.com>
2025-01-20 13:38:27 -0800

    refactor driver code

--- main.go
+++ main.go
@@ -1,46 +1,62 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"log"
-	"net"
+	"strconv"
+	"time"
 )
 
 func main() {
-	addr := &net.TCPAddr{
-		IP:   net.ParseIP("127.0.0.1"),
-		Port: 3000,
-	}
-	// fsp := NewFsSnapShotStore[PersistedRaftFields]("test")
-	// cfg := RaftNodeConfig{
-	// 	Id:              100,
-	// 	Address:         addr,
-	// 	ElectionTimeout: 100 * time.Millisecond,
-	// 	HeartbeatPeriod: 100 * time.Millisecond,
-	// 	StateMachine:    &StorageStateMachine{Storer: Store{}},
-	// 	RaftTransport:   &RaftTransport{},
-	// 	DataManager:     &DataManager{},
-	// 	SnapshotStorer:  fsp,
-	// 	Cluster:         make([]PeerRaftNode, 0),
-	// 	logs:            make([]proto.LogEntry, 0),
+	// dp := NewDataPlane(":4000")
+	// go dp.Listen()
+	// time.Sleep(500 * time.Millisecond)
+	// ds := NewDataSender()
+	// data := "momsbestpicture"
+	// buf := bytes.NewBuffer([]byte(data))
+	// err := ds.Send(io.NopCloser(buf), "http://localhost:4000/p2p/transfer")
+	// if err != nil {
+	// 	fmt.Println(err)
 	// }
-	// raftnode := NewRaftNode(cfg)
-	// raftnode.Start()
-	tr := NewDataTransport(addr)
-	go func() {
-		for {
-			select {
-			case tcpData := <-tr.Consume():
-				data, err := io.ReadAll(tcpData.r)
-				if err != nil {
-					log.Println(err)
-				}
-				// fmt.Printf("%s", data)
-				fmt.Println(string(data))
-				tcpData.wg.Done()
+	const (
+		KB          = 1024
+		MB          = 1024 * KB
+		sizeInBytes = 50 * MB
+	)
+	nameNode := NewNameNode(0, ":3000", nil, 3*time.Second, 5)
+	NUMNODES := 5
+	MAXSIMCOMMANDS := 5
+	dataNodes := make([]DataNode, NUMNODES+1)
+	peerDataNodes := make([]PeerDataNode, NUMNODES+1)
+	for i := 1; i < (NUMNODES + 1); i++ {
+		address := ":300" + strconv.Itoa(i)
+		storeOpts := StoreOpts{
+			blockSize: 5,
+			root:      address,
+		}
+		dataNodes[i] = *NewDataNode(uint64(i), address, NewStore(storeOpts), 50*MB)
+		peerDataNodes[i] = *dataNodes[i].PeerRepresentation()
+	}
+
+	nameNode.AddDataNodes(peerDataNodes)
+
+	for i := 1; i < (NUMNODES + 1); i++ {
+		dataNodes[i].NameNode = *nameNode.PeerRepresentation()
+
+		// Create peer list excluding self
+		peers := make([]PeerDataNode, 0)
+		for j := 1; j < (NUMNODES + 1); j++ {
+			if j != i { // Don't add self as peer
+				peers = append(peers, peerDataNodes[j])
 			}
 		}
-	}()
-	tr.Start()
+		dataNodes[i].AddDataNodes(peers)
+
+		dataNodes[i].InitializeClients()
+		go func(idx int) { // Pass i as parameter
+			dataNodes[idx].StartRPCServer()
+			defer dataNodes[idx].CloseAllClients()
+		}(i)
+	}
+
+	time.Sleep(time.Second * 500)
+
 }
