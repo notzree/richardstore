78212cded0d88ebb6bc3ace0efd9a7b0ff94fa2e
notzree <richard@symphoniclabs.com>
2025-03-14 23:54:21 -0700

    refactor DataNode
    refactor DataNode to adhere to new proto schema. Condensed writing and
    replication rpc handlers and simplified message data payload types.

--- hdfs/data-node.go
+++ hdfs/data-node.go
@@ -60,7 +60,7 @@ const (
 
 type DataNode struct {
 	Id          uint64
-	port        string
+	address     string
 	MAX_RETRIES int
 
 	state   DataNodeState
@@ -77,6 +77,9 @@ type DataNode struct {
 	errChan  chan error
 	doneChan chan interface{}
 
+	cLogMu    *sync.Mutex
+	ChangeLog []*proto.FileUpdate
+
 	// InitialHeartbeatInterval   time.Duration
 	// InitialBlockReportInterval time.Duration
 	proto.UnimplementedDataNodeServer
@@ -90,10 +93,9 @@ func NewDataNode(Id uint64, address string, storer *str.Store, maxSimCommands in
 	} else {
 		nodeCapacity = storer.GetAvailableCapacity()
 	}
-	log.Printf("INITIALIZING NODE WITH %v CAPACITY", nodeCapacity)
 	return &DataNode{
 		Id:          Id,
-		port:        address,
+		address:     address,
 		Storer:      storer,
 		statMu:      &sync.Mutex{},
 		Capacity:    nodeCapacity,
@@ -116,6 +118,7 @@ func (node *DataNode) Run() error {
 	}
 	go node.HandleHeartbeat()
 	for {
+		// stay in Safe Mode until node.state is active
 		node.stateMu.RLock()
 		if node.state == Active {
 			break
@@ -123,6 +126,7 @@ func (node *DataNode) Run() error {
 		node.stateMu.RUnlock()
 		time.Sleep(1 * time.Second)
 	}
+	log.Printf("NODE ACTIVE!")
 	go node.StartRPCServer()
 	go node.HandleCommands()
 	go node.HandleBlockReport()
@@ -136,14 +140,14 @@ func (node *DataNode) Run() error {
 }
 
 func (node *DataNode) StartRPCServer() error {
-	ln, err := net.Listen("tcp", node.port)
+	ln, err := net.Listen("tcp", node.address)
 	if err != nil {
 		return err
 	}
 	opts := []grpc.ServerOption{}
 	server := grpc.NewServer(opts...)
 	proto.RegisterDataNodeServer(server, node)
-	log.Printf("starting data node server on port %s\n", node.port)
+	log.Printf("starting data node server on port %s\n", node.address)
 	go func() {
 		server.Serve(ln)
 	}()
@@ -152,29 +156,42 @@ func (node *DataNode) StartRPCServer() error {
 	return nil
 }
 
-// handle replication stream (datanode -> peer datanode)
-func (node *DataNode) ReplicateFile(stream grpc.ClientStreamingServer[proto.ReplicateFileRequest, proto.CommandResponse]) error {
-	// Get the first message containing the command
+// write file and handle replication stream (datanode -> peer datanode)
+func (node *DataNode) WriteFile(stream grpc.ClientStreamingServer[proto.FileStream, proto.WriteFileResponse]) error {
 	firstMsg, err := stream.Recv()
 	if err != nil {
 		return fmt.Errorf("failed to receive initial command: %w", err)
 	}
 
-	repCmd, ok := firstMsg.Request.(*proto.ReplicateFileRequest_Command)
+	streamType, ok := firstMsg.Type.(*proto.FileStream_StreamInfo)
 	if !ok {
-		return fmt.Errorf("first message must be a command")
+		return fmt.Errorf("first message must contain stream info!")
 	}
 
-	cmd := repCmd.Command
-	expectedFileHash := cmd.FileInfo.Hash
+	info := streamType.StreamInfo
+	expectedFileHash := info.FileInfo.Hash
+
+	var replicator *FilestreamReplicator
+
+	if len(info.DataNodes) >= 0 {
+		downstreamNodeInfo := info.DataNodes[0]
+		downstreamInfo := &proto.StreamInfo{
+			FileInfo:  info.FileInfo,
+			DataNodes: info.DataNodes[1:],
+		}
+		ctx := context.Background()
+		replicator, err = NewReplicator(ctx, downstreamNodeInfo.Address, downstreamInfo)
+		if err != nil {
+			return fmt.Errorf("failed to initiate replication chain %v", err)
+		}
+	}
 
 	// Create a pipe to connect the stream to an io.Reader
 	pr, pw := io.Pipe()
 
 	// Start a goroutine to read from the stream and write to the pipe
 	go func() {
-		defer pw.Close() // Make sure we close the writer when done
-
+		defer pw.Close()
 		for {
 			msg, err := stream.Recv()
 			if err == io.EOF {
@@ -185,7 +202,7 @@ func (node *DataNode) ReplicateFile(stream grpc.ClientStreamingServer[proto.Repl
 				return
 			}
 
-			chunk, ok := msg.Request.(*proto.ReplicateFileRequest_Chunk)
+			chunk, ok := msg.Type.(*proto.FileStream_Chunk)
 			if !ok {
 				pw.CloseWithError(fmt.Errorf("expected chunk message"))
 				return
@@ -196,97 +213,57 @@ func (node *DataNode) ReplicateFile(stream grpc.ClientStreamingServer[proto.Repl
 				pw.CloseWithError(fmt.Errorf("error writing to pipe: %w", err))
 				return
 			}
+			// write chunk to downstream node if exist
+			if replicator != nil {
+				replicator.SendChunk(&proto.FileStream{
+					Type: chunk,
+				})
+			}
 		}
 	}()
-
-	// Pass the reader end of the pipe to Write()
 	hash, err := node.Storer.Write(pr)
 	if err != nil {
 		return fmt.Errorf("failed to write file: %w", err)
 	}
-	if hash != expectedFileHash {
-		return fmt.Errorf("file hash mismatch expecte %s got %s", expectedFileHash, hash)
-	}
-	if len(cmd.TargetNodes) > 0 {
-		node.cmdChan <- &proto.Command{
-			Command: &proto.Command_Replicate{
-				Replicate: cmd,
-			},
-		}
-	}
-	log.Printf("File replication completed for node %d", node.Id)
-	return stream.SendAndClose(&proto.CommandResponse{
-		Success: true,
-	})
-}
-
-func (node *DataNode) WriteFile(stream grpc.ClientStreamingServer[proto.WriteFileRequest, proto.WriteFileResponse]) error {
-	firstMsg, err := stream.Recv()
+	response, err := replicator.CloseAndRecv()
 	if err != nil {
-		return fmt.Errorf("failed to receive initial command: %w", err)
+		return fmt.Errorf("err closing replication stream: %v", err)
 	}
-	resp, ok := firstMsg.Request.(*proto.WriteFileRequest_FileInfo)
-	if !ok {
-		return fmt.Errorf("first message must be a command")
-	}
-
-	expectedFileHash := resp.FileInfo.Hash
-
-	// Create a pipe to connect the stream to an io.Reader
-	pr, pw := io.Pipe()
-
-	// Start a goroutine to read from the stream and write to the pipe
-	go func() {
-		defer pw.Close() // Make sure we close the writer when done
-
-		for {
-			msg, err := stream.Recv()
-			if err == io.EOF {
-				return
-			}
-			if err != nil {
-				pw.CloseWithError(fmt.Errorf("error receiving chunk: %w", err))
-				return
-			}
-
-			chunk, ok := msg.Request.(*proto.WriteFileRequest_Chunk)
-			if !ok {
-				pw.CloseWithError(fmt.Errorf("expected chunk message"))
-				return
-			}
-
-			_, err = pw.Write(chunk.Chunk)
-			if err != nil {
-				pw.CloseWithError(fmt.Errorf("error writing to pipe: %w", err))
-				return
-			}
-		}
-	}()
-
-	// Pass the reader end of the pipe to Write()
-	hash, err := node.Storer.Write(pr)
-	if err != nil {
-		return fmt.Errorf("failed to write file: %w", err)
+	if !response.Success {
+		// TODO: Should we delete this to avoid partial failures?
+		return fmt.Errorf("failed to replicate to downstream nodes %v", err)
 	}
 	if hash != expectedFileHash {
-		return fmt.Errorf("file hash mismatch expecte %s got %s", expectedFileHash, hash)
+		err := node.Storer.Delete(hash) // don't save corrupt data
+		didDelete := true
+		if err != nil {
+			didDelete = false
+			fmt.Errorf("failed to orphaned file with hash %v", hash)
+		}
+		return fmt.Errorf("file hash mismatch expecte %s got %s | did delete?:", expectedFileHash, hash, didDelete)
 	}
 
-	log.Printf("wrote file %s to node %d", hash, node.Id)
 	node.statMu.Lock()
 	defer node.statMu.Unlock()
-	node.Used += resp.FileInfo.Size
-	stream.SendAndClose(&proto.WriteFileResponse{
-		Success: true,
+	node.Used += info.FileInfo.Size
+
+	node.cLogMu.Lock()
+	defer node.cLogMu.Unlock()
+	node.ChangeLog = append(node.ChangeLog, &proto.FileUpdate{
+		FileInfo: info.FileInfo,
+		Update:   *proto.FileUpdate_UPDATE_ADD.Enum(),
 	})
 
-	return nil
+	log.Printf("File replication completed for node %d", node.Id)
+	return stream.SendAndClose(&proto.WriteFileResponse{
+		Success: true,
+	})
 }
 
-func (node *DataNode) ReadFile(req *proto.ReadFileRequest, stream grpc.ServerStreamingServer[proto.ReadFileStream]) error {
+func (node *DataNode) ReadFile(req *proto.ReadFileRequest, stream grpc.ServerStreamingServer[proto.FileStream]) error {
 	file, err := node.Storer.Read(req.Hash)
 	if err != nil {
-		return err
+		return fmt.Errorf("err opening file on node %v | err: %v", node.address, err)
 	}
 	defer file.Close()
 
@@ -295,12 +272,15 @@ func (node *DataNode) ReadFile(req *proto.ReadFileRequest, stream grpc.ServerStr
 		return err
 	}
 	// Send Read File Info as first message
-	err = stream.Send(&proto.ReadFileStream{
-		Request: &proto.ReadFileStream_FileInfo{
-			FileInfo: &proto.DataNodeFile{
-				Hash:              req.Hash,
-				Size:              uint64(fileInfo.Size()),
-				ModificationStamp: uint64(fileInfo.ModTime().Unix()),
+	err = stream.Send(&proto.FileStream{
+		Type: &proto.FileStream_StreamInfo{
+			StreamInfo: &proto.StreamInfo{
+				FileInfo: &proto.FileInfo{
+					Hash:              req.Hash,
+					Size:              uint64(fileInfo.Size()),
+					ModificationStamp: uint64(fileInfo.ModTime().Unix()),
+				},
+				DataNodes: []*proto.DataNodeInfo{}, //replication param unused
 			},
 		},
 	})
@@ -308,8 +288,7 @@ func (node *DataNode) ReadFile(req *proto.ReadFileRequest, stream grpc.ServerStr
 		return err
 	}
 
-	// Stream the rest of the data over in a buffer
-	buf := make([]byte, 1024)
+	buf := make([]byte, 512*1024)
 	for {
 		_, err := file.Read(buf)
 		if err == io.EOF {
@@ -318,8 +297,8 @@ func (node *DataNode) ReadFile(req *proto.ReadFileRequest, stream grpc.ServerStr
 		if err != nil {
 			return err
 		}
-		err = stream.Send(&proto.ReadFileStream{
-			Request: &proto.ReadFileStream_Chunk{
+		err = stream.Send(&proto.FileStream{
+			Type: &proto.FileStream_Chunk{
 				Chunk: buf,
 			},
 		})
@@ -338,7 +317,7 @@ func (node *DataNode) SendHeartbeat() (time.Duration, error) {
 		NodeId:   node.Id,
 		Capacity: node.Capacity,
 		Used:     node.Used,
-		Address:  node.port, //TODO: Might need to change this, should be aight thru docker-compose tho
+		Address:  node.address,
 	})
 	if err != nil {
 		return 0, err
@@ -349,19 +328,13 @@ func (node *DataNode) SendHeartbeat() (time.Duration, error) {
 
 	return time.Duration(response.NextHeartbeatDelay), nil
 }
+
 func (node *DataNode) HandleHeartbeat() {
 	// max consecutive retries
 	MAX_RETRIES := node.MAX_RETRIES
-	interval, err := node.SendHeartbeat()
-	if err != nil {
-		log.Printf("err sending heartbeat %s", err)
-		node.sendError(err)
-		return
-	}
-	node.stateMu.Lock()
-	node.state = Active
-	node.stateMu.Unlock()
+	interval := 5 * time.Second
 	ticker := time.NewTicker(interval)
+	success := false
 	defer ticker.Stop()
 	for {
 		select {
@@ -376,6 +349,15 @@ func (node *DataNode) HandleHeartbeat() {
 					node.sendError(err)
 					return
 				}
+			}
+			if !success {
+				node.stateMu.Lock()
+				if node.state == SafeMode {
+					log.Printf("First successful heartbeat, transitioning to Active state")
+					node.state = Active
+				}
+				node.stateMu.Unlock()
+				success = true
 			}
 			MAX_RETRIES += 1 // Reset retry count on successful heartbeat
 			if newInterval != interval {
@@ -393,13 +375,13 @@ func (node *DataNode) SendBlockreport() (time.Duration, error) {
 	if err != nil {
 		return 0, err
 	}
-	heldFiles := make([]*proto.DataNodeFile, len(rawFiles))
+	heldFiles := make([]*proto.FileInfo, len(rawFiles))
 	for i, file := range rawFiles {
 		fileInfo, err := file.Stat()
 		if err != nil {
 			return 0, err
 		}
-		heldFiles[i] = &proto.DataNodeFile{
+		heldFiles[i] = &proto.FileInfo{
 			Hash:              fileInfo.Name(),
 			Size:              uint64(fileInfo.Size()),
 			ModificationStamp: uint64(fileInfo.ModTime().Unix()),
@@ -417,9 +399,6 @@ func (node *DataNode) SendBlockreport() (time.Duration, error) {
 	})
 	if err != nil {
 		return 0, err
-	}
-	for _, cmd := range response.Commands {
-		node.cmdChan <- cmd
 	}
 	return time.Duration(response.NextReportDelay), nil
 
@@ -466,6 +445,66 @@ func (node *DataNode) HandleBlockReport() {
 	}
 }
 
+func (node *DataNode) HandleIncrementalBlockreport() {
+	// max consecutive retries
+	MAX_RETRIES := node.MAX_RETRIES
+	interval := 10 * time.Second // Default interval for initial failure case
+	// Try first block report
+	newInterval, err := node.SendIncrementalBlockReport()
+	if err != nil {
+		log.Printf("err sending initial block report %s", err)
+		MAX_RETRIES -= 1
+	} else {
+		interval = newInterval
+	}
+
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-node.doneChan:
+			return
+		case <-ticker.C:
+			newInterval, err := node.SendIncrementalBlockReport()
+			if err != nil {
+				log.Println(err)
+				MAX_RETRIES -= 1
+				if MAX_RETRIES <= 0 {
+					node.sendError(err)
+					return
+				}
+			} else {
+				MAX_RETRIES = node.MAX_RETRIES // Reset retry count on success
+				if newInterval != interval {
+					ticker.Reset(newInterval)
+					interval = newInterval
+				}
+			}
+		}
+	}
+
+}
+func (node *DataNode) SendIncrementalBlockReport() (time.Duration, error) {
+	// minimize lock contention
+	node.cLogMu.Lock()
+	logs := node.ChangeLog
+	node.ChangeLog = make([]*proto.FileUpdate, 0)
+	node.cLogMu.Unlock()
+
+	c := node.NameNode.Client
+	ctx := context.Background()
+	resp, err := c.client.IncrementalBlockReport(ctx, &proto.IncrementalBlockReportRequest{
+		NodeId:  node.Id,
+		Updates: logs,
+	})
+	if err != nil {
+		return 0, nil
+	}
+	return time.Duration(resp.NextReportDelay), nil
+
+}
+
 // Handle + process any commands
 func (node *DataNode) HandleCommands() {
 	for {
@@ -498,6 +537,12 @@ func (node *DataNode) handleDelete(cmd *proto.DeleteCommand) error {
 	if err != nil {
 		return err
 	}
+	node.cLogMu.Lock()
+	defer node.cLogMu.Unlock()
+	node.ChangeLog = append(node.ChangeLog, &proto.FileUpdate{
+		FileInfo: cmd.FileInfo,
+		Update:   *proto.FileUpdate_UPDATE_DELETE.Enum(),
+	})
 	// might want to grab the file size again from the file system rather than trusting cmd
 	node.statMu.Lock()
 	defer node.statMu.Unlock()
@@ -522,36 +567,20 @@ func (node *DataNode) handleReplication(cmd *proto.ReplicateCommand) error {
 		log.Printf("end of replication chain reached with node %d", node.Id)
 		return nil
 	}
-	target := cmd.TargetNodes[0]
-	remaining := cmd.TargetNodes[1:]
 
+	target := cmd.TargetNodes[0]
+	downstreamInfo := &proto.StreamInfo{
+		FileInfo:  cmd.FileInfo,
+		DataNodes: cmd.TargetNodes[1:],
+	}
 	ctx := context.Background()
-	peerNode, err := NewDataNodeClient(target.Address)
-	defer peerNode.Close()
+	replicator, err := NewReplicator(ctx, target.Address, downstreamInfo)
 	if err != nil {
 		return err
 	}
-	stream, err := peerNode.client.ReplicateFile(ctx)
-	if err != nil {
-		return fmt.Errorf("failed to create stream: %w", err)
-	}
-	log.Printf("sending first message to node: %d\n", target)
-	// First send the command
-	err = stream.Send(&proto.ReplicateFileRequest{
-		Request: &proto.ReplicateFileRequest_Command{
-			Command: &proto.ReplicateCommand{
-				FileInfo:                 cmd.FileInfo,
-				TargetNodes:              remaining, // Pass remaining nodes
-				CurrentReplicationFactor: cmd.CurrentReplicationFactor,
-			},
-		},
-	})
-	if err != nil {
-		return fmt.Errorf("failed to send command: %w", err)
-	}
 
 	// Then stream the file chunks
-	buf := make([]byte, 32*1024) // 32KB chunks
+	buf := make([]byte, 512*1024) // 512KB chunks
 	for {
 		n, err := fr.Read(buf)
 		if err == io.EOF {
@@ -561,8 +590,8 @@ func (node *DataNode) handleReplication(cmd *proto.ReplicateCommand) error {
 			return fmt.Errorf("error reading file: %w", err)
 		}
 
-		err = stream.Send(&proto.ReplicateFileRequest{
-			Request: &proto.ReplicateFileRequest_Chunk{
+		err = replicator.SendChunk(&proto.FileStream{
+			Type: &proto.FileStream_Chunk{
 				Chunk: buf[:n],
 			},
 		})
@@ -571,8 +600,7 @@ func (node *DataNode) handleReplication(cmd *proto.ReplicateCommand) error {
 		}
 	}
 
-	// Close the stream and get response
-	response, err := stream.CloseAndRecv()
+	response, err := replicator.CloseAndRecv()
 	if err != nil {
 		return fmt.Errorf("error closing stream: %w", err)
 	}
@@ -596,9 +624,11 @@ func (node *DataNode) InitializeClients() error {
 }
 
 func (node *DataNode) PeerRepresentation() *PeerDataNode {
+	node.statMu.Lock()
+	defer node.statMu.Unlock()
 	return &PeerDataNode{
 		Id:       node.Id,
-		Address:  node.port,
+		Address:  node.address,
 		Alive:    true,
 		LastSeen: time.Now(),
 		// in bytes
